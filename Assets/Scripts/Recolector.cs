9b76701a5dc6e492774d8ef0ce85e811d8661ea4
ErickG11 <grandaerick8@gmail.com>
2025-05-07 17:27:13 -0500

    Commit Final

---
+++ Assets/Scripts/Recolector.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Recolector : MonoBehaviour
+{
+    public int contador = 0;
+
+    public void RecolectarObjeto()
+    {
+        contador++;
+        Debug.Log("Objeto recolectado. Total: " + contador);
+    }
+}
+
